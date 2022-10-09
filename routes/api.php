b6702e83e131eb0fb4ab0b73b401c9e926467b8d
zulkarnainspj <zulemail.sepanjang@gmail.com>
2022-10-09 08:50:33 +0700

    data kapal

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\Auth\LoginController;
 use App\Http\Controllers\Admin\Auth\LogoutController;
+use App\Http\Controllers\Admin\Data\KapalController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -21,3 +22,5 @@ date_default_timezone_set('Asia/Jakarta');
 
 Route::post('/admin/login', LoginController::class)->name('login');
 Route::post('/admin/logout', LogoutController::class);
+Route::get('/admin/kapal', [KapalController::class, 'index']);
+Route::post('/admin/kapal/store', [KapalController::class, 'store']);
