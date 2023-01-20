cc62d72da36e348c5120c4402dcd8f3310d82326
zulkarnainspj <zulemail.sepanjang@gmail.com>
2023-01-21 01:44:17 +0700

    perbaikan tampilan data penumpang

--- resources/views/admin/passengers/index.blade.php
+++ resources/views/admin/passengers/index.blade.php
@@ -1,64 +1,54 @@
 @extends('admin.layout.main')
 @section('container')
-    <div class="content-wrapper">
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Penumpang</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="/">Home</a></li>
-                            <li class="breadcrumb-item active">Penumpang</li>
-                        </ol>
-                    </div>
-                </div>
-        </section>
-        <section class="content">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="card">
-                            <div class="card">
-                                <div class="card-body">
-                                    <table id="example1" class="table table-bordered table-striped">
-                                        <thead>
-                                            <tr class="text-center">
-                                                <th>#</th>
-                                                <th>Nama</th>
-                                                <th>HP</th>
-                                                <th>Email</th>
-                                                <th>Action</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($passengers as $item)
-                                                <tr>
-                                                    <td class="text-center">{{ $loop->iteration }}</td>
-                                                    <td>{{ $item->name }}</td>
-                                                    <td class="text-center">{{ $item->phone }}</td>
-                                                    <td class="text-center">{{ $item->email }}</td>
-                                                    <td align="center">
-                                                        <a href="/admin/passengers/edit/{{ $item->id }}"
-                                                            class="btn btn-sm btn-warning text-light" title="Edit"><span
-                                                                class="fas fa-fw fa-edit"></span></a>
-                                                        <a href="/admin/passengers/remove/{{ $item->id }}"
-                                                            onclick="return confirm('Are you sure you want to remove this data?');"
-                                                            class="btn btn-sm btn-danger" title="Remove"><span
-                                                                class="fas fa-fw fa-trash"></span></a>
+    <!-- Breadcrumbs-->
+    <div class="bg-white border-bottom py-3 mb-3">
+        <div
+            class="container-fluid d-flex justify-content-between align-items-start align-items-md-center flex-column flex-md-row">
+            <nav class="mb-0" aria-label="breadcrumb">
+                <ol class="breadcrumb m-0">
+                    <li class="breadcrumb-item"><a href="/">Home</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Penumpang</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+    <!-- / Breadcrumbs-->
 
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
+    <section class="container-fluid mb-3">
+        <h2 class="fs-3 fw-bold mt-1">Penumpang</h2>
+
+        <div class="row g-4">
+            <div class="col-md-12">
+                <div class="card h-100">
+                    <div class="card-body">
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                                <tr class="text-center">
+                                    <th>#</th>
+                                    <th>Nama</th>
+                                    <th>HP</th>
+                                    <th>Email</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($passengers as $item)
+                                    <tr>
+                                        <td class="text-center">{{ $loop->iteration }}</td>
+                                        <td>{{ $item->name }}</td>
+                                        <td class="text-center">{{ $item->phone }}</td>
+                                        <td class="text-center">{{ $item->email }}</td>
+                                        <td align="center">
+                                            <a href="/admin/passengers/{{ $item->id }}"
+                                                class="btn btn-sm btn-primary" title="Lihat Detail"><i class="bi bi-eye-fill"></i></a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
 @endsection
